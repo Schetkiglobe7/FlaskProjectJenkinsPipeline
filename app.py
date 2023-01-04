d47b3b80d57a1eae02cdb0e65628e771ce73ea3a
Antonio <antonio.pilato@bcsoft.net>
2023-01-04 12:24:12 +0100

    prova rilascio dopo integrazione jenkins

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_guys():
-    return '<h1>Hello from Flask!</h1>'
+    return '<h1>Hello from Flask</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
